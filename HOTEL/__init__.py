65e83bbe23fbf0b23b8cb32fc0f14cd2e5e82741
avni <avniisrani9@gmail.com>
2025-02-16 05:16:14 -0800

    Add elements/style to signup page

---
+++ HOTEL/__init__.py
@@ -0,0 +1,15 @@
+from flask import render_template, Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.route("/signup")
+def sign_up():
+    return render_template('signup.html')
+
+@app.route("/login")
+def log_in():
+    return render_template('login.html')
