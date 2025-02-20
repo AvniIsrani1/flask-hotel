d5dae4b093d72a0fee996f8c92adda5743234a8e
avni <avniisrani9@gmail.com>
2025-02-20 02:03:06 -0800

    Improve mobile styles for base page. Add dashboard toggle option to bookings page. Create and style profile page.

--- HOTEL/__init__.py
+++ HOTEL/__init__.py
@@ -14,6 +14,10 @@ def sign_up():
 def log_in():
     return render_template('login.html')
 
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
 @app.route("/my-bookings")
 def my_bookings():
     return render_template('bookings.html')
