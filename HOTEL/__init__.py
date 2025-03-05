47e2a53818bda8c9f786792193322cd696cc6db2
avni <avniisrani9@gmail.com>
2025-03-04 18:52:35 -0800

    Create base styling for faq page. Modify base page redirections. Write table initialization sql queries.

--- HOTEL/__init__.py
+++ HOTEL/__init__.py
@@ -1,8 +1,49 @@
-from flask import render_template, Flask
+from flask import Flask, render_template
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
-@app.route("/")
+# def connecting():
+#     connection = None
+#     try:
+#         # Connecting to MySQL database 
+#         connection = mysql.connector.connect(
+#             host="localhost",
+#             user="root",  
+#             password="admin",  
+#             database="testdb"  
+#         )
+#         if connection.is_connected():
+#                 return connection
+#     except Error as e:
+#         print(f"Error: {e}")
+#     return None
+
+#initalize tables in database
+# def run_sql_script():
+#     connection = connecting()
+#     if connection is None:
+#         print("Not able to connect. Exiting...")
+#         return
+#     try:
+#         cursor = connection.cursor()
+#         print("Connection established")
+#         with open('./sql/initial.sql','r') as file:
+#             sql_file = file.read()
+#         lines = sql_file.split(';')
+#         for line in lines:
+#             if line.strip():
+#                 cursor.execute(line.strip())
+#         connection.commit()
+#         print("Done initializing tables")
+
+#     except Error as e:
+#         print(f"Error executing SQL script: {e}")
+#     finally:
+#         connection.close()
+
+@app.route('/')
 def home():
     return render_template('index.html')
 
@@ -18,14 +59,24 @@ def log_in():
 def profile():
     return render_template('profile.html')
 
-@app.route("/my-bookings")
-def my_bookings():
-    return render_template('bookings.html')
-
 @app.route("/request-services")
 def request_services():
     return render_template('request_services.html')
 
+@app.route("/bookings")
+def bookings():
+    return render_template('bookings.html')
+
 @app.route("/search")
 def search():
     return render_template('search.html')
+
+@app.route("/faq")
+def faq():
+    return render_template('faq.html')
+
+
+if __name__ == "__main__":
+    # run_sql_script()
+    app.run(debug=True)
+
