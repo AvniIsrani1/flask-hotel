9a511524a311d63eec46d31f7a8123828188d1c5
avni <avniisrani9@gmail.com>
2025-03-19 19:40:31 -0700

    Store reservation details in session instead so user can reload without error. Add 'my bookings' page to navigation

--- HOTEL/__init__.py
+++ HOTEL/__init__.py
@@ -294,38 +294,45 @@ def reserve():
     startdate = None
     enddate = None
     if request.method=='POST':
-        rid = request.form.get('rid')
-        location_type = request.form.get('location-type')
-        startdate = request.form.get('startdate')
-        enddate = request.form.get('enddate')
-        print(f"Received rid: {rid}, location_type: {location_type}, startdate: {startdate}, enddate: {enddate}")  # Debugging
+        session["rid"] = rid = request.form.get('rid')
+        session["location_type"] = location_type = request.form.get('location-type')
+        session["startdate"] = startdate = request.form.get('startdate')
+        session["enddate"] = enddate = request.form.get('enddate')
         if not startdate or not enddate:
             flash('Please enter both the start and end dates',"error")
             return redirect(url_for('search'))
-        query = Room.query.join(Hotel).filter(Room.available==Availability.A)
-        # query = query.group_by(
-        #     Room.hid, Room.room_type, Room.number_beds, Room.rate, Room.balcony, Room.city_view, Room.ocean_view, 
-        #     Room.smoking, Room.max_guests, Room.wheelchair_accessible
-        # )
-        starting = datetime.strptime(str(startdate), "%B %d, %Y").replace(hour=0,minute=0,second=0)
-        ending = datetime.strptime(str(enddate), "%B %d, %Y").replace(hour=23,minute=59,second=59)
-        duration = (ending - starting).days + 1
-        room = query.filter(Room.id==rid).first()
-        similar_rooms = Room.query.join(Hotel).filter(
-            Room.hid==room.hid, Room.room_type==room.room_type, Room.number_beds==room.number_beds, Room.rate==room.rate, Room.balcony==room.balcony, Room.city_view==room.city_view,
-            Room.ocean_view==room.ocean_view, Room.smoking==room.smoking, Room.max_guests==room.max_guests, Room.wheelchair_accessible==room.wheelchair_accessible
-        )
-        similar_rooms = similar_rooms.filter(not_(db.exists().where(Booking.rid == Room.id).where(Booking.check_in < ending).where(Booking.check_out>starting)))
-        similar_rooms = similar_rooms.group_by(
-            Room.hid, Room.room_type, Room.number_beds, Room.rate, Room.balcony, Room.city_view, Room.ocean_view, 
-            Room.smoking, Room.max_guests, Room.wheelchair_accessible
-        )
-        similar_rooms = similar_rooms.with_entities(Room, Hotel.address, func.count(distinct(Room.id)).label('number_rooms'), func.min(Room.id).label('min_rid'))
-        room = similar_rooms.first()
-        if not room:
-            print('room not found')
-            flash('Room not found',"error")
-            return redirect(url_for('home'))
+    rid = session.get("rid")
+    location_type = session.get("location_type")
+    startdate = session.get("startdate")
+    enddate = session.get("enddate")
+    print(f"Received rid: {rid}, location_type: {location_type}, startdate: {startdate}, enddate: {enddate}") 
+    if not rid or not startdate or not enddate:
+        flash("Reservation details are missing. Please search for a room again.", "error")
+        return redirect(url_for('search'))
+    query = Room.query.join(Hotel).filter(Room.available==Availability.A)
+    # query = query.group_by(
+    #     Room.hid, Room.room_type, Room.number_beds, Room.rate, Room.balcony, Room.city_view, Room.ocean_view, 
+    #     Room.smoking, Room.max_guests, Room.wheelchair_accessible
+    # )
+    starting = datetime.strptime(str(startdate), "%B %d, %Y").replace(hour=0,minute=0,second=0)
+    ending = datetime.strptime(str(enddate), "%B %d, %Y").replace(hour=23,minute=59,second=59)
+    duration = (ending - starting).days + 1
+    room = query.filter(Room.id==rid).first()
+    similar_rooms = Room.query.join(Hotel).filter(
+        Room.hid==room.hid, Room.room_type==room.room_type, Room.number_beds==room.number_beds, Room.rate==room.rate, Room.balcony==room.balcony, Room.city_view==room.city_view,
+        Room.ocean_view==room.ocean_view, Room.smoking==room.smoking, Room.max_guests==room.max_guests, Room.wheelchair_accessible==room.wheelchair_accessible
+    )
+    similar_rooms = similar_rooms.filter(not_(db.exists().where(Booking.rid == Room.id).where(Booking.check_in < ending).where(Booking.check_out>starting)))
+    similar_rooms = similar_rooms.group_by(
+        Room.hid, Room.room_type, Room.number_beds, Room.rate, Room.balcony, Room.city_view, Room.ocean_view, 
+        Room.smoking, Room.max_guests, Room.wheelchair_accessible
+    )
+    similar_rooms = similar_rooms.with_entities(Room, Hotel.address, func.count(distinct(Room.id)).label('number_rooms'), func.min(Room.id).label('min_rid'))
+    room = similar_rooms.first()
+    if not room:
+        print('room not found')
+        flash('Room not found',"error")
+        return redirect(url_for('home'))
     return render_template('reserve.html', user=user, room=room, YesNo=YesNo, rid=rid, location_type=location_type, duration=duration, startdate=startdate, enddate=enddate)
 
 @app.route("/terms")
