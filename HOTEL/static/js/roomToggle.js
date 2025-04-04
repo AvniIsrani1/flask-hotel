3dfecdf6f6210de9c584b437c727fdad6b6639a1
avni <avniisrani9@gmail.com>
2025-04-03 21:24:03 -0700

    Fix image sizing in search and bookings.

--- HOTEL/static/js/roomToggle.js
+++ HOTEL/static/js/roomToggle.js
@@ -2,9 +2,11 @@
 function showModel(roomId) {
     document.getElementById(`thumbnail-${roomId}`).style.display = 'none';
     document.getElementById(`model-container-${roomId}`).style.display = 'block';
+    document.getElementById(`item-image-overlay-${roomId}`).style.display = 'none';
 }
 
 function hideModel(roomId) {
     document.getElementById(`model-container-${roomId}`).style.display = 'none';
     document.getElementById(`thumbnail-${roomId}`).style.display = 'block';
+    document.getElementById(`item-image-overlay-${roomId}`).style.display = 'flex';
 } 
