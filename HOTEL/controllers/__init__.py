500ace689db66191e72353d482a9371df28e0232
avni <avniisrani9@gmail.com>
2025-04-17 17:38:10 -0700

    Create FormController class for Form interactions.

--- HOTEL/controllers/__init__.py
+++ HOTEL/controllers/__init__.py
@@ -1,5 +1,6 @@
 from .RoomAvailability import RoomAvailability
 from .SearchController import SearchController
 from .EmailController import EmailController
+from .FormController import FormController
 
-__all__ = ['RoomAvailability','SearchController','EmailController']
+__all__ = ['RoomAvailability','SearchController','EmailController', 'FormController']
